cd06aa4c56be51b9086a3c57bd1238afceac9a2e
Dylan Trochain <dylvn-dev@pm.me>
2026-04-11 22:17:07 +0200

    Add FeatureToggle to enable/disable front faq page

---
+++ src/Dylvn/Bundle/FaqBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dylvn\Bundle\FaqBundle\DependencyInjection;
+
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('dylvn_faq');
+        $rootNode = $treeBuilder->getRootNode();
+
+        SettingsBuilder::append(
+            $rootNode,
+            [
+                'faq_feature_enabled' => ['value' => true, 'type' => 'boolean'],
+            ]
+        );
+
+        return $treeBuilder;
+    }
+}
--- src/Dylvn/Bundle/FaqBundle/DependencyInjection/DylvnFaqExtension.php
+++ src/Dylvn/Bundle/FaqBundle/DependencyInjection/DylvnFaqExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Dylvn\Bundle\FaqBundle\DependencyInjection;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -11,8 +12,11 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class DylvnFaqExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
+        $config = $this->processConfiguration($this->getConfiguration($configs, $container), $configs);
+        $container->prependExtensionConfig($this->getAlias(), SettingsBuilder::getSettings($config));
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('controllers.yml');
         $loader->load('form_types.yml');
